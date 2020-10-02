5dda6eb85150b6ef11a23159f161968466e38479
Genghius <62076069+Genghius@users.noreply.github.com>
2020-10-02 22:48:21 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ rofi -v 2> /dev/null 1> /dev/null || echo "rofi $yikes"&& sudo apt install -y ro
 awk -V 2> /dev/null 1> /dev/null || echo "awk $yikes" && sudo apt install -y mawk
 feh -v 2> /dev/null 1> /dev/null || echo "feh $yikes" && sudo apt install -y feh
 scrot -v 2> /dev/null 1> /dev/null || echo "scrot $yikes" && sudo apt install -y scrot
-dmenu -v 2> /dev/null 1> /dev/null || echo "dmenu $yikes" && sudo apt install -y dmneu
+dmenu -v 2> /dev/null 1> /dev/null || echo "dmenu $yikes" && sudo apt install -y dmenu
 i3blocks -V 2> /dev/null 1> /dev/null || echo "i3blocks $yikes" && sudo apt install -y i3blocks
 pulseaudio --version 2> /dev/null 1> /dev/null || echo "pulseaudio $yikes" && sudo apt install -y pulseaudio
 
