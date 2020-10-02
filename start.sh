0c5f7f3404330c973111d167bbac57311f143428
Genghius <62076069+Genghius@users.noreply.github.com>
2020-10-02 23:07:37 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,15 +8,16 @@ dmenu -v 2> /dev/null 1> /dev/null || echo "dmenu $yikes" && sudo apt install -y
 i3blocks -V 2> /dev/null 1> /dev/null || echo "i3blocks $yikes" && sudo apt install -y i3blocks
 pulseaudio --version 2> /dev/null 1> /dev/null || echo "pulseaudio $yikes" && sudo apt install -y pulseaudio
 
+user=$(who | awk '{print $1}')
 echo "proceeding to create directories, copy and overwrite dotfiles"
 ls /usr/share/i3blocks/ 2> /dev/null 1> /dev/null || sudo mkdir /usr/share/i3blocks
-ls /home/*/.config/i3 2> /dev/null 1> /dev/null || mkdir /home/*/.config/i3
-ls /home/*/.config/rofi 2> /dev/null 1> /dev/null || mkdir /home/*/.config/rofi
+ls /home/$user/.config/i3 2> /dev/null 1> /dev/null || mkdir /home/$user/.config/i3
+ls /home/$user/.config/rofi 2> /dev/null 1> /dev/null || mkdir /home/$user/.config/rofi
 sudo cp -r $PWD/dotfiles/blocks/* /usr/share/i3blocks
 sudo cp -r $PWD/dotfiles/i3blocks.conf
-cp -r $PWD/dotfiles/config /home/*/.config/i3
-cp -r $PWD/misc/HK.jpeg /home/*/.config/i3
-cp -r $PWD/dotfiles/config.rasi /home/*/.config/rofi
+cp -r $PWD/dotfiles/config /home/$user/.config/i3
+cp -r $PWD/misc/HK.jpeg /home/$user/.config/i3
+cp -r $PWD/dotfiles/config.rasi /home/$user/.config/rofi
 
 echo "done, proceeding to delete script and related garbage."
 trap "rm -rf $PWD" EXIT
