54d6c6bb4e901b17bb5ea62c2cdcc373889ff11a
Genghius <genghius.khanate01@gmail.com>
2020-10-14 00:31:04 +0100

    literally just fucking copy pasted arch branch lol

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
-# Ubuntu-i3-configurer
+# Arch-i3-configurer
 
-This script will configure an i3 instalation in ubuntu, linux mint or else; with a dark theme and a basic set of config files.
+This script will configure an i3 instalation in Ubuntu based distro, like Linux Mint; Dark theme and a basic set of config files included.
 
-all keybinding, configuration files and else can be found after instalation in:
-~/.config/i3
-~/.config/rofi
-/etc
-/usr/share/i3blocks
-
+all keybinding, configuration files and else can be found after instalation in:\
+~/.config/i3\
+~/.config/rofi\
+~/.config/i3blocks
 
 ### Must be run as sudo to work
+
+# Features added
+Better commented code\
+Little corrections. For example, fixed a bug that cause a package installs twice\
+A brief description after install\
+us-intl keyboard layout to write symbols outside English language easier\
+Unnecesary code and i3blocks removed\
+Detect user command shorter\
+All dotfiles now uses ~/ (home) directories only instead of /etc and weird locations\
+100% shell language\
+thanks to ronycreator
+
--- start.sh
+++ start.sh
@@ -1,32 +1,51 @@
+# Check if command is running as root
 if [ "$EUID" -ne 0 ]
-  then echo "please run the script as root. with:         sudo bash start.sh"
+  then echo "Please start the script as root with: sudo ./start.sh"
   exit
 fi
-yikes="not installed, proceeding to install..."
-echo "Checking that all required packages are installed..."
-rofi -v 2> /dev/null 1> /dev/null || (echo "rofi $yikes"&& sudo apt install -y rofi 1> /dev/null)
-awk -V 2> /dev/null 1> /dev/null || (echo "awk $yikes" && sudo apt install -y mawk 1> /dev/null)
-feh -v 2> /dev/null 1> /dev/null || (echo "feh $yikes" && sudo apt install -y feh 1> /dev/null)
-sensors -v 2> /dev/null 1> /dev/null || (echo "sensors $yikes" && sudo apt install -y lm-sensors 1> /dev/null)
-mpstat -V 2> /dev/null 1> /dev/null || (echo "mpstat $yikes" && sudo apt install -y sysstat 1> /dev/null)
-scrot -v 2> /dev/null 1> /dev/null || (echo "scrot $yikes" && sudo apt install -y scrot 1> /dev/null)
-dmenu -v 2> /dev/null 1> /dev/null || (echo "dmenu $yikes" && sudo apt install -y dmenu 1> /dev/null)
-i3blocks -V 2> /dev/null 1> /dev/null || (echo "i3blocks $yikes" && sudo apt install -y i3blocks 1> /dev/null)
-pulseaudio --version 2> /dev/null 1> /dev/null || (echo "pulseaudio $yikes" && sudo apt install -y pulseaudio 1> /dev/null)
 
+# Variable for commands below
+yikes="is not installed, proceeding to install..."
+
+# Check required packages and install them if necessary
+echo "Checking that all required packages are installed..."
+rofi -v 2> /dev/null 1> /dev/null || (echo "rofi $yikes" && sudo apt install rofi 1> /dev/null)
+awk -V 2> /dev/null 1> /dev/null || (echo "awk $yikes" && sudo apt install mawk 1> /dev/null)
+feh -v 2> /dev/null 1> /dev/null || (echo "feh $yikes" && sudo apt install feh 1> /dev/null)
+sensors -v 2> /dev/null 1> /dev/null || (echo "sensors $yikes" && sudo apt install lm-sensors 1> /dev/null)
+mpstat -V 2> /dev/null 1> /dev/null || (echo "mpstat $yikes" && sudo apt install sysstat 1> /dev/null)
+scrot -v 2> /dev/null 1> /dev/null || (echo "scrot $yikes" && sudo apt install scrot 1> /dev/null)
+dmenu -v 2> /dev/null 1> /dev/null || (echo "dmenu $yikes" && sudo apt install dmenu 1> /dev/null)
+i3blocks -V 2> /dev/null 1> /dev/null || (echo "i3blocks $yikes" && sudo apt install i3blocks 1> /dev/null)
+pulseaudio --version 2> /dev/null 1> /dev/null || (echo "pulseaudio $yikes" && sudo apt install pulseaudio 1> /dev/null)
+
+# More checking
 echo "checking i3 or i3-gaps is installed..."
 i3 -v 2> /dev/null 1> /dev/null || (echo "i3-gaps $yikes" && sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake libxcb-xrm-dev 1> /dev/null && git clone https://www.github.com/Airblader/i3 i3-gaps && cd i3-gaps && git checkout gaps && git pull && autoreconf --force --install && rm -rf build && mkdir build && cd build && ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers && make && sudo make install && cd ..)
 
-user=$(who | awk 'NR==1 {print $1}')
-echo "proceeding to create directories, copy and overwrite dotfiles"
-ls /usr/share/i3blocks/ 2> /dev/null 1> /dev/null || sudo mkdir /usr/share/i3blocks
+# Finds the user and store in a variable
+# Can alternatively use $(who -m | awk 'RN==1 {print $1})'
+user=$(logname)
+
+# Copy dotfiles
+# Create directories
+echo "Creating directories to copy and overwrite dotfiles if do not exist"
+ls /home/$user/.config/i3blocks 2> /dev/null 1> /dev/null || mkdir /home/$user/.config/i3blocks
 ls /home/$user/.config/i3 2> /dev/null 1> /dev/null || mkdir /home/$user/.config/i3
 ls /home/$user/.config/rofi 2> /dev/null 1> /dev/null || mkdir /home/$user/.config/rofi
-sudo cp -r $PWD/dotfiles/blocks/* /usr/share/i3blocks
-sudo cp -r $PWD/dotfiles/i3blocks.conf /etc
+
+# Really copy dotfiles
+echo "Copying dotfiles to respective locations"
+cp -r $PWD/dotfiles/blocks/* /home/$user/.config/i3blocks
+cp -r $PWD/dotfiles/i3blocks.conf /home/$user/.config/i3blocks/config
 cp -r $PWD/dotfiles/config /home/$user/.config/i3
 cp -r $PWD/misc/HK.jpg /home/$user/.config/i3
 cp -r $PWD/dotfiles/config.rasi /home/$user/.config/rofi
 
+# Print instructions
+echo "To edit configs, start editing with your favorite editor ~/.config/i3/config and /home/$user/.config/i3blocks files. Check containing folders also to know the structure and do more things... Happy Ricing"
+
+# Deleting this cloned repo
+#echo "NO"
 echo "done, proceeding to delete script and related garbage."
 rm -rf $PWD
