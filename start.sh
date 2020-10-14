32209d662465c5c022454a53708f1d065834b792
Genghius <genghius.khanate03@gmail.com>
2020-10-14 11:49:35 +0100

    added dunst notification daemon.

--- start.sh
+++ start.sh
@@ -9,15 +9,16 @@ yikes="is not installed, proceeding to install..."
 
 # Check required packages and install them if necessary
 echo "Checking that all required packages are installed..."
-rofi -v 2> /dev/null 1> /dev/null || (echo "rofi $yikes" && sudo apt install rofi 1> /dev/null)
-awk -V 2> /dev/null 1> /dev/null || (echo "awk $yikes" && sudo apt install mawk 1> /dev/null)
-feh -v 2> /dev/null 1> /dev/null || (echo "feh $yikes" && sudo apt install feh 1> /dev/null)
-sensors -v 2> /dev/null 1> /dev/null || (echo "sensors $yikes" && sudo apt install lm-sensors 1> /dev/null)
-mpstat -V 2> /dev/null 1> /dev/null || (echo "mpstat $yikes" && sudo apt install sysstat 1> /dev/null)
-scrot -v 2> /dev/null 1> /dev/null || (echo "scrot $yikes" && sudo apt install scrot 1> /dev/null)
-dmenu -v 2> /dev/null 1> /dev/null || (echo "dmenu $yikes" && sudo apt install dmenu 1> /dev/null)
-i3blocks -V 2> /dev/null 1> /dev/null || (echo "i3blocks $yikes" && sudo apt install i3blocks 1> /dev/null)
-pulseaudio --version 2> /dev/null 1> /dev/null || (echo "pulseaudio $yikes" && sudo apt install pulseaudio 1> /dev/null)
+rofi -v 2> /dev/null 1> /dev/null || (echo "rofi $yikes" && sudo apt install -y rofi 1> /dev/null 2> /dev/null )
+awk -V 2> /dev/null 1> /dev/null || (echo "awk $yikes" && sudo apt install -y mawk 1> /dev/null 2> /dev/null )
+feh -v 2> /dev/null 1> /dev/null || (echo "feh $yikes" && sudo apt install -y feh 1> /dev/null 2> /dev/null )
+sensors -v 2> /dev/null 1> /dev/null || (echo "sensors $yikes" && sudo apt install -y lm-sensors 1> /dev/null 2> /dev/null )
+mpstat -V 2> /dev/null 1> /dev/null || (echo "mpstat $yikes" && sudo apt install -y sysstat 1> /dev/null 2> /dev/null )
+scrot -v 2> /dev/null 1> /dev/null || (echo "scrot $yikes" && sudo apt install -y scrot 1> /dev/null 2> /dev/null )
+dmenu -v 2> /dev/null 1> /dev/null || (echo "dmenu $yikes" && sudo apt install -y dmenu 1> /dev/null 2> /dev/null )
+i3blocks -V 2> /dev/null 1> /dev/null || (echo "i3blocks $yikes" && sudo apt install -y i3blocks 1> /dev/null 2> /dev/null )
+pulseaudio --version 2> /dev/null 1> /dev/null || (echo "pulseaudio $yikes" && sudo apt install -y pulseaudio 1> /dev/null 2> /dev/null )
+dunst -v 2> /dev/null 1> /dev/null || (echo "dunst $yikes" && sudo apt install -y dunst 1> /dev/null 2> /dev/null )
 
 # More checking
 echo "checking i3 or i3-gaps is installed..."
@@ -33,6 +34,7 @@ echo "Creating directories to copy and overwrite dotfiles if do not exist"
 ls /home/$user/.config/i3blocks 2> /dev/null 1> /dev/null || mkdir /home/$user/.config/i3blocks
 ls /home/$user/.config/i3 2> /dev/null 1> /dev/null || mkdir /home/$user/.config/i3
 ls /home/$user/.config/rofi 2> /dev/null 1> /dev/null || mkdir /home/$user/.config/rofi
+ls /home/$user/.config/dunst 2> /dev/null 1> /dev/null || mkdir /home/$user/.config/dunst
 
 # Really copy dotfiles
 echo "Copying dotfiles to respective locations"
@@ -41,6 +43,7 @@ cp -r $PWD/dotfiles/i3blocks.conf /home/$user/.config/i3blocks/config
 cp -r $PWD/dotfiles/config /home/$user/.config/i3
 cp -r $PWD/misc/HK.jpg /home/$user/.config/i3
 cp -r $PWD/dotfiles/config.rasi /home/$user/.config/rofi
+cp -r $PWD/dotfiles/dunstrc /home/$user/.config/dunst
 
 # Print instructions
 echo "To edit configs, start editing with your favorite editor ~/.config/i3/config and /home/$user/.config/i3blocks files. Check containing folders also to know the structure and do more things... Happy Ricing"
